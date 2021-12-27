ddcbb8b3cc9fcfbdaa12614f1a90fe24beeaf248
jowoojeon <wjwj0916@hanmail.net>
2021-12-27 18:47:01 +0900

    제발 돼라,,
    제발,,

--- script/script.js
+++ script/script.js
@@ -4,14 +4,14 @@ $(function(){
             $(".slide li").eq(0).appendTo(".slide");
             $(".slide").css("top",0)
         })
-    },4000)
-    
-})
+    },4000);
+});
+
 var mobileKeyWords = new Array('iPhone', 'iPod', 'BlackBerry', 'Android', 'Windows CE', 'Windows CE;', 'LG', 'MOT', 'SAMSUNG', 'SonyEricsson', 'Mobile', 'Symbian', 'Opera Mobi', 'Opera Mini', 'IEmobile');
 for (var word in mobileKeyWords){
     if (navigator.userAgent.match(mobileKeyWords[word]) != null){
         window.location.href = "/mobile";
         break;
-    }
-}
+    };
+};
 
